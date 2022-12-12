c9564e6fb8dc29062fa1a2ec2451f67bc0e4bc01
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-12-12 13:17:12 +0300

    feat: implement hash solution

--- node-nodejs-basics/src/hash/calcHash.js
+++ node-nodejs-basics/src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import path, { resolve } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const pathToHashForFile = resolve(path.dirname(''), 'src', 'hash', 'files', 'fileToCalculateHashFor.txt')
+    const fileBuffer = fs.readFileSync(pathToHashForFile, (err) => {
+        if (err) throw err;
+    });
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
+    console.log(hex);
+
 };
 
 await calculateHash();
