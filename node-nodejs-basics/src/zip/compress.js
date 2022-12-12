52e6503b5229f7776cc9c984ece7c58d5f2271dd
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-12-12 15:46:24 +0300

    feat: implement zip solutions

--- node-nodejs-basics/src/zip/compress.js
+++ node-nodejs-basics/src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { resolve } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const pathToCompressFile = resolve(path.dirname(''), 'src', 'zip', 'files', 'fileToCompress.txt');
+    const pathToCompressedFile = resolve(path.dirname(''), 'src', 'zip', 'files', 'archive.gz');
+    const gzip = zlib.createGzip();
+    const rstream = fs.createReadStream(pathToCompressFile);
+    const wstream = fs.createWriteStream(pathToCompressedFile);
+
+    rstream.pipe(gzip).pipe(wstream).on('finish', () => {
+        console.log('FINISHED!');
+    })
 };
 
 await compress();
--- node-nodejs-basics/src/zip/decompress.js
+++ node-nodejs-basics/src/zip/decompress.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { resolve } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+    const pathToCompressFile = resolve(path.dirname(''), 'src', 'zip', 'files', 'fileToCompress.txt');
+    const pathToCompressedFile = resolve(path.dirname(''), 'src', 'zip', 'files', 'archive.gz');
+    const gunzip = zlib.createGunzip();
+    const rstream = fs.createReadStream(pathToCompressedFile);
+    const wstream = fs.createWriteStream(pathToCompressFile);
+
+    rstream.pipe(gunzip).pipe(wstream).on('finish', () => {
+        console.log('FINISHED!');
+    })
 };
 
 await decompress();
