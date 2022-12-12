ad64a4fa8a1bbae3514004cb21d94e3e1cb38720
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2022-12-12 12:28:35 +0300

    feat: implement command line interface solutions

--- node-nodejs-basics/src/cli/args.js
+++ node-nodejs-basics/src/cli/args.js
@@ -1,5 +1,10 @@
-const parseArgs = () => {
-    // Write your code here 
+import { argv } from 'node:process';
+
+export const parseArgs = () => {
+    const filtered = argv.filter(arg => !arg.match(/C:/gi));
+    for (let i = 0; i < filtered.length; i += 2) {
+        console.log(`${filtered[i]} is ${filtered[i + 1]}`);
+    }
 };
 
 parseArgs();
